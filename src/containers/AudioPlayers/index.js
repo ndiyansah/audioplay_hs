1bb7bef6652eb31c9a194a4ae5472ed8c6c1c3a5
handi sulyansah <sulyansah94handi@gmail.com>
2021-04-13 00:06:35 +0700

    feat(search): add component search and integrated api

--- src/containers/AudioPlayers/index.js
+++ src/containers/AudioPlayers/index.js
@@ -1,28 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { MainWrapper } from '../../theme/MainStyle/Wrapper';
-import ItunesList  from '../../components/ItunesList';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { MainWrapper, Header } from "../../theme/MainStyle/Wrapper";
+import ItunesSearch from "../../components/ItunesSearch";
+import ItunesList from "../../components/ItunesList";
 
 const AudioPlayers = () => {
+  const [search, setSearch] = useState('')
   const [data, setData] = useState({ hits: [] });
-  useEffect(() => {
-    const fetchData = async () => {
+
+  const fetchSearch = async () => {
       const result = await axios(
-        `https://itunes.apple.com/search?term=pamungkas`,
+        `https://itunes.apple.com/search?term=${search}`
       );
       setData(result.data);
     };
- 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `https://itunes.apple.com/search?term=pamungkas`
+      );
+      setData(result.data);
+    };
+
     fetchData();
   }, []);
- 
+
+console.log(search)
   return (
-    <MainWrapper>
-      <h1>Hello World</h1>
-      <ItunesList
-        data={data}
-       />
-    </MainWrapper>
+    <>
+      <Header>
+        <ItunesSearch 
+          value={search}
+          onChange={(val) => setSearch(val)}
+          onSearch={() => {
+          fetchSearch({})
+        }}
+        placeholder="Artists and Songs"
+        />
+      </Header>
+
+      <MainWrapper>
+        <ItunesList data={data} />
+      </MainWrapper>
+    </>
   );
 };
 
